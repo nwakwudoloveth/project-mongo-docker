f012e61f56da909194265613a6c49731d05fd119
Loveth Nwakwudo <nwakwudoloveth@gmail.com>
2022-12-11 14:03:44 +0000

    get request

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,47 +5,27 @@ const User = require('../models/user')
 const Product = require('../models/product')
 
 router.get('/initialise', async function (req, res) {
-  console.log('---------------------------')
   const shoe = new Product({ image: 'url', price: 1500, description: 'This is a good shoe' })
   shoe.save()
-  const mariana = new User({ name: 'mariana', email: 'mariana@gmail.com', password: 'password' })
-  const loveth = new User({ name: 'Loveth', email: 'loveth@gmail.com', password: 'password' })
-
-  const zeynep = new User({ name: 'Zeynep', email: 'Zeynep@gmail.com', password: 'password' })
-
+  const mariana = User.create({ name: 'mariana', email: 'mariana@gmail.com', password: 'password' })
+  const loveth = User.create({ name: 'Loveth', email: 'loveth@gmail.com', password: 'password' })
+  const zeynep = User.create({ name: 'Zeynep', email: 'Zeynep@gmail.com', password: 'password' })
   zeynep.list(shoe)
   loveth.list(shoe)
   mariana.list(shoe)
   await mariana.save()
   await loveth.save()
   await zeynep.save()
-  // const users = [mariana, loveth, zeynep]
-  console.log(zeynep)
-  console.log(loveth)
-  console.log(mariana)
   res.sendStatus(200)
-  // res.send(users)
+})
+// get users Request
+router.get('/', async function (req, res, next) {
+  try {
+    const users = await User.find({})
+    res.render('user', { users })
+  } catch (e) {
+    res.sendStatus(404)
+  }
 })
 
-// router.get('/', async function (req, res) {
-//   const result = await User.find({})
-
-//   // if (req.query.name) {
-//   //   result = U.filter(user => user.name === req.query.name)
-//   // }
-//   res.send(result)
-// })
-router.get('/', async function (req, res) {
-  const users = await User.find({})
-  if (users) res.render('user', { users })
-  else res.sendStatus(404)
-  // res.send(users)
-})
-
-router.post('/', async function (req, res) {
-  const { name, email, password } = req.body
-
-  const user = await User.create({ name, email, password })
-  res.send(user)
-})
 module.exports = router
